c53706d85b20dc6c92677f92d379c5d1faed2664
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2025-05-29 21:34:29 +0200

    Tabulation handling using PrimeReact

--- Client/src/components/OptionTab.tsx
+++ Client/src/components/OptionTab.tsx
@@ -1,38 +1,46 @@
-import React from 'react'
-import styles from './OptionTab.module.css'
-import TabContent from './TabContent'
 import EditUser from './EditUser'
 import DeleteUser from './DeleteUser'
 import CreateUser from './CreateUser'
-import TabButton from './TabButton'
+import { TabView, TabPanel } from 'primereact/tabview';
 
 const OptionTab = () => {
-  const [activeTab, setActiveTab] = React.useState('Create');
-
-  const openTab = (tabName: string) => {
-    setActiveTab(tabName);
-  }
-
   return (
-    <>
-      <div className={styles.tab}>
-        <TabButton label="Create" name="Create" activeName={activeTab} onClick={openTab} />
-        <TabButton label="Edit" name="Edit" activeName={activeTab} onClick={openTab} />
-        <TabButton label="Delete" name="Delete" activeName={activeTab} onClick={openTab} />
-      </div>
+    <TabView>
+      <TabPanel header="Create">
+        <div className="m-0">
+          <CreateUser />
+        </div>
+      </TabPanel>
+      <TabPanel header="Edit">
+        <div className="m-0">
+          <EditUser />
+        </div>
+      </TabPanel>
+      <TabPanel header="Delete">
+        <div className="m-0">
+          <DeleteUser />
+        </div>
+      </TabPanel>
+    </TabView>
+    // <>
+    //   <div className={styles.tab}>
+    //     <TabButton label="Create" name="Create" activeName={activeTab} onClick={openTab} />
+    //     <TabButton label="Edit" name="Edit" activeName={activeTab} onClick={openTab} />
+    //     <TabButton label="Delete" name="Delete" activeName={activeTab} onClick={openTab} />
+    //   </div>
 
-      <TabContent name="Create" activeName={activeTab}>
-        <CreateUser />
-      </TabContent>
+    //   <TabContent name="Create" activeName={activeTab}>
+    //     <CreateUser />
+    //   </TabContent>
 
-      <TabContent name="Edit" activeName={activeTab}>
-        <EditUser />
-      </TabContent>
+    //   <TabContent name="Edit" activeName={activeTab}>
+    //     <EditUser />
+    //   </TabContent>
 
-      <TabContent name="Delete" activeName={activeTab}>
-        <DeleteUser />
-      </TabContent>
-    </>
+    //   <TabContent name="Delete" activeName={activeTab}>
+    //     <DeleteUser />
+    //   </TabContent>
+    // </>
   )
 }
 
--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { PrimeReactProvider } from "primereact/api";
 import './index.css'
+import "primereact/resources/themes/bootstrap4-light-blue/theme.css"
 import App from './App.tsx'
 
 const queryClient = new QueryClient();
@@ -9,7 +11,9 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <PrimeReactProvider>
+        <App />
+      </PrimeReactProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
